c739e44c01c4b12a4fe8eabc97b096e36ff60d03
Andrii Mazur <andreymazur93@gmail.com>
2023-02-10 16:35:17 +0100

    Update streamlit_app.py
    adds multiselect widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ streamlit.title('Some fancy streamlit app')
 
 import pandas as pd
 
+# read data
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list)
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# display the table on the page
+streamlit.dataframe(my_fruit_list)
