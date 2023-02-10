5e6ded25fd6443edc21a0249659487872a4db836
Andrii Mazur <andreymazur93@gmail.com>
2023-02-10 16:33:41 +0100

    Update streamlit_app.py
    reads ands displays a dataframe from s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('Some fancy streamlit app')
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
+
