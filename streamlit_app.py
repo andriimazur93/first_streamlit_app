62d98fac11f1ee6bbaac5ff9fbe3aef6874564b2
Andrii Mazur <andreymazur93@gmail.com>
2023-02-10 16:39:13 +0100

    Update streamlit_app.py
    Adds new feature that only selected fruits will be displayed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,17 @@
 import streamlit
+import pandas as pd
+
 
 streamlit.title('Some fancy streamlit app')
 
-import pandas as pd
-
 # read data
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
