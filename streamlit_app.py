1322ce5d12f16adf7380144693857c9d3a507df7
Andrii Mazur <andreymazur93@gmail.com>
2023-02-11 10:26:07 +0100

    Update streamlit_app.py
    refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,26 @@
 import streamlit
 import pandas as pd
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('Some fancy streamlit app')
 
 # read data
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-
+# New section to display fruityvice api response
 streamlit.title('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
 fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 
 # take the json version of the response and normalize it
@@ -39,3 +38,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
