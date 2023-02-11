758ee7139e9fd6d9345648a3fd7c2af98a1341f7
Andrii Mazur <andreymazur93@gmail.com>
2023-02-11 09:42:13 +0100

    Update streamlit_app.py
    adds dynamic user input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,11 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.title('Fruityvice Fruit Advice!')
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
